0cbc38863dd8422040c79f9404d6947357499d7e
Marius <marius.siauciulis@gmail.com>
2017-08-26 21:07:54 +0100

    Ported most of the remaining functions, NOT TESTED

--- YeeBulb.py
+++ YeeBulb.py
@@ -110,7 +110,7 @@ class YeeBulb:
 				YeeBulb.display("Unexpected error:" + e)
 				return (False, e)
 
-	def get_state(self, req_params = []):
+	def get_state(self, req_params):
 		"""	Method to retrieve current state of specified bulb parameters	"""
 		params = ""
 		for i in range(0, len(req_params)):
@@ -175,7 +175,6 @@ class YeeBulb:
 		else:
 			return (False, "Parameters out of range")
 	
-	#NOT TESTED
 	def turn_on(self, effect = "sudden", duration = 30):
 		""" Method to turn on the bulb. """
 		params = "\"on\"" + ",\"" + str(effect) + "\"," + str(duration)
@@ -190,11 +189,12 @@ class YeeBulb:
 		""" Toggles on/off. """
 		self.operate("toggle", "")
 
+	#NOT TESTED
 	def set_default(self):
 		"""Sets current bulb state as default. """
 		self.operate("set_default", "")
 
-	def start_cf(self, count, action, flow_expression):
+	def start_cf(self, count, action, *flow_expressions):
 		"""
 		This method is used to start a color flow. Color flow is a series of smart
 		LED visible state changing. It can be brightness changing, color changing or color
@@ -207,8 +207,115 @@ class YeeBulb:
 				0 means smart LED recover to the state before the color flow started.
 				1 means smart LED stay at the state when the flow is stopped.
 				2 means turn off the smart LED after the flow is stopped.
-			flow_expression: the expression of the state changing series.
-		
+			flow_expression: the expression of the state changing series. [duration, mode, value, brightness]
+				duration: Gradual change time or sleep time in milliseconds, minimum value 50.
+				mode: 1 – color, 2 – color temperature, 7 -- sleep.
+				value: RGB value when mode is 1, CT value when mode is 2, ignored when mode is 7.
+				brightness: Brightness value, 1 ~ 100. Ignored when mode is 7.
 		Request Example: 
 		{"id":1,"method":"start_cf","params":[ 4, 2, "1000, 2, 2700, 100, 500, 1,255, 10, 5000, 7, 0,0, 500, 2, 5000, 1"]
 		"""
+		#TODO check if "flow_expressions" corelate with "count"
+		params = str(count) +"," + str(action)
+		for expression in flow_expressions
+			params += ',' + expression
+		return self.operate("start_cf" )
+	
+	def stop_cf(self)
+	""" Method to stop the color flow """
+		return self.operate("stop_cf", "")
+	
+	def set_scene(self, class_type, *args)
+	 """
+	 This method is used to set the smart LED directly to specified state.
+	 If the smart LED is off, then it will turn on the smart LED firstly and then apply the specified command
+	 
+	Args:
+		class_type: "color", "hsv", "ct", "cf", "auto_dealy_off".
+			"color": change the smart LED to specified color and brightness.
+			"hsv": change the smart LED to specified color and brightness.
+			"ct": change the smart LED to specified ct and brightness.
+			"cf": start a color flow in specified fashion.
+			"auto_delay_off": turn on the smart LED to specified brightness and start a sleep timer to turn off the light after the specified time
+		args: class specific.
+	 """
+	 #TODO check if arg is in range for specific class_type
+	 class_list =["color", "hsv", "ct", "cf", "auto_delay_off"]
+	 if class_type in class_list:
+		 params = str(class_type)
+		 for arg in args:
+			 param += str(arg) + ','
+		return self.operate("set_scene", params)
+	else:
+		return (False, "Parameters out of range")
+	
+	def cron_add(self, value, mode = 0)
+		"""
+		This method is used to start a timer job on the smart LED.
+		Args:	
+			value: is the length of the timer (in minutes).
+			mode: currently can only be 0. (means power off)
+		"""
+		params = str(mode) + ',' +str(value)
+		return self.operate("cron_add", params)
+
+	def cron_get(self, mode = 0)
+	"""
+	This method is used to retrieve the setting of the current cron job of the specified type. 
+	Args:
+		mode: type of the cron job. (currently only support 0).
+	"""
+	return self.operate("cron_get", str(mode))
+
+	def cron_del(self, mode)
+	"""
+	This method is used to stop the specified cron job.
+	Args:
+		mode: the type of the cron job. (currently only support 0).
+	"""
+	return self.operate("cron_del", str(mode))
+
+	def set_adjust(self, prop, action = "circle")
+	"""
+	This method is used to change brightness, CT or color of a smart LED without knowing the current value.
+	Args:
+		action: direction of the adjustment. The valid values:
+			“increase": increase the specified property
+			“decrease": decrease the specified property
+			“circle": increase the specified property, after it reaches the max value, go back to minimum value.
+		prop: property to adjust. The valid values:
+			“bright": adjust brightness.
+			“ct": adjust color temperature.
+			“color": adjust color. (When “prop" is “color", the “action" can only be “circle", otherwise, it will be deemed as invalid request.)
+	"""
+	params = action + ',' + prop
+	return self.operate("set_adjust", params)
+
+	def set_music(self, action, host, port)
+	"""
+	This method is used to start or stop music mode on a device. Under music mode, no property will be reported and no message quota is checked.
+	Args:
+		action: action of set_music command. The valid values:
+			0: turn off music mode.
+			1: turn on music mode.
+		host: IP address of the music server.
+		port: TCP port music application is listening on.
+
+	Note:
+		When control device wants to start music mode, it needs start a TCP 
+		server firstly and then call “set_music” command to let the device know the IP and Port of the
+		TCP listen socket. After the command is received, LED device will try to connect the specified
+		peer address. If the TCP connection is established successfully, then control device can
+		send all supported commands through this channel without any limits to simulate any music effect.
+		The control device can stop music mode by explicitly sending a stop command or by closing the socket.
+	"""
+	pass
+
+	def set_name(self, name)
+	"""
+	This method is used to name the device. The name will be stored on the device and reported in discovering response.
+	User can also read the name through “get_prop” method.
+	Args:
+		name: new name of the bulb
+	"""
+	return self.operate("set_name", str(name))
